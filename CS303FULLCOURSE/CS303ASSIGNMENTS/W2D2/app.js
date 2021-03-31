4e212fef6b916fde9f85d33de47c98c341140dd3
frekassa2101 <69208220+frekassa2101@users.noreply.github.com>
2021-03-31 09:25:07 -0400

    updated

---
+++ CS303FULLCOURSE/CS303ASSIGNMENTS/W2D2/app.js
@@ -0,0 +1,29 @@
+"use strict";
+/**
+ * 
+ * @param {object} arr array 
+ * @param {function} fun function
+ * @returns {object} object
+ */
+function myMap(arr, fun) {
+    let result = fun(arr);
+    return result;
+}
+/**
+ * @param{Array} arr to
+ * @param {Object} fun nu
+ * @return{Array} arr
+ */
+function myFilter(arr, fun) {
+    let result = fun(arr);
+    return result;
+}/**
+ * 
+ * @param {Array} arr to
+ * @param {Object} fun to
+ * @return {Array} to
+ */
+function myReduce(arr, fun) {
+    let result = fun(arr);
+    return result;
+}
