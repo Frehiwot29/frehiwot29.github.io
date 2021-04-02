662bc09a995f77f68fc66d7022fe4554618a6743
frekassa2101 <69208220+frekassa2101@users.noreply.github.com>
2021-04-01 23:24:45 -0400

    updated

--- CS303FULLCOURSE/CS303ASSIGNMENTS/W2D4/tree.js
+++ CS303FULLCOURSE/CS303ASSIGNMENTS/W2D4/tree.js
@@ -1,6 +1,5 @@
 "use strict";
 /**
- * 
  * @param {String} value to
  * @return{String} value
  */
@@ -8,7 +7,6 @@ function TreeNode(value) {
     this.value = value;
     this.descendant = [];
     return this.value;
-
 }
 // create nodes with values
 const abe = new TreeNode("Abe");
@@ -19,19 +17,14 @@ const maggie = new TreeNode("Maggie");
 abe.descendant.push(homer);
 homer.descendant.push(bart, lisa, maggie);
 //Q1.
-// console.log(abe.value);
-// console.log(homer.value);
-// console.log(bart.value);
-// console.log(lisa.value);
-// console.log(maggie.value);
+console.log(abe.value);
+console.log(homer.value);
+console.log(bart.value);
+console.log(lisa.value);
+console.log(maggie.value);
 //Q2.
-
-let search1 = "lisa";
+let search1 = "Lisa";
 let search2 = "crush";
-//console.log(this.value(item => item === search1));
-//console.log(value(item => item === search2));
-//console.log(contains(=== search2));
-let tree = new TreeNode();
-console.log(tree.descendant === this.search2);
-console.log(tree.descendant === this.search1);
-console.log(tree.descendant);
+
+console.log(lisa.value === search1);
+console.log(bart.value === search1);
