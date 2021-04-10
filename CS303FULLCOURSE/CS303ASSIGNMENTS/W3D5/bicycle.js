330e3d012fa9a918300be7d4fca186eca7dcce87
frekassa2101 <69208220+frekassa2101@users.noreply.github.com>
2021-04-09 22:46:05 -0400

    Updated

--- CS303FULLCOURSE/CS303ASSIGNMENTS/W3D5/bicycle.js
+++ CS303FULLCOURSE/CS303ASSIGNMENTS/W3D5/bicycle.js
@@ -4,47 +4,34 @@
 // Java program to illustrate the
 // concept of inheritance
 // base class
-let bicycle = {
-    // the Bicycle class has two fields
-    gear: 2,
-    speed: 4,
-    // the Bicycle class has three methods
-    applyBrake: function () {
+const bicycle = {
+    gear: 1,
+    speed: 0,
+    speedUp: function (increment) {
+        this.speed += increment;
+    },
+    applyBrake: function (decrement) {
         this.speed -= decrement;
     },
-    speedUp: function () {
-        this.speed += increment;
-    }
 };
-// the Bicycle class has one constructor
-function Bicycle(gear, speed) {
-    this.gear = gear;
-        this.speed = speed;
-}
-let bicyc = new Bicycle(gear, speed);
-bicyc.applyBrake();
-bicyc.speedUp();
-// derived class
-mountainBike.__proto__ = bicycle;
-// the MountainBike subclass adds one more field
-mountainBike.seatHeight;
-// setHeight:function(){
-//     this.seatHeight=newValue;
-// }
-// the MountainBike subclass has one constructor
-function MountainBike(gear, speed, startHeight) {
-    // invoking base-class(Bicycle) constructor
-    this.gear = gear;
-    this.speed = speed;
-    this.seatHeight = startHeight;
-};
-let mountanB = new MountainBike(gear, speed, startHeight);
-mountanB.speedUp(100);
-mountanB.applyBrake(123);
-// the MountainBike subclass adds one more method
-let setHeight = {
-    seatHeight: "newValue"
-}
-console.log(mountanB.applyBrake(1000));
 
+console.log("bicycle object: ", bicycle);
+let mountainBike = {
+    gear: 3,
+    speed: 2   //102-10 = 92
+}
+
+mountainBike.__proto__ = bicycle; //[[property]]
+
+//bicycle.type = "Nike";
+//mountainBike.type = "Bike"
+console.log(mountainBike.speedUp(100));
+console.log(mountainBike.applyBrake(10));
+
+console.log(mountainBike.speed);
+console.log(mountainBike.gear); //Nike
+console.log("mountainBike object", mountainBike);
+//console.log("mountainBike object", bicycle);
+
+//console.log(bicycle.prototype);
 
